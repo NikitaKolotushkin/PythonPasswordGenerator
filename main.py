614f1becb79df0ac0df98990d209b8ea246e4b7c
NikitaKolotushkin <nikitaplay1000@gmail.com>
2020-08-19 21:14:05 +0400

    Added basic functionality

--- main.py
+++ main.py
@@ -2,3 +2,22 @@
 # -*- coding: utf-8 -*-
 
 
+import random
+
+
+password_length = int(input("Please enter the required password length: "))
+
+
+def main(length=password_length):
+
+    symbols = "1234567890QqWwEeRrTtYyUuIiOoPpAaSsDdFfGgHhJjKkLlZzXxCcVvBbNnMm"
+    
+    password = ""
+
+    for i in range(password_length):
+        password += random.choice(symbols)
+
+    return password
+
+if __name__ == "__main__":
+    print(main())
